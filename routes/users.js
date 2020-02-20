e3660055a62d7dfa88be6b43e271ddbb07cc3172
MAIRTIN O FLATHARTA(19-09-2019 13-26 <danu7_om1@danu7>
2020-02-20 12:36:55 +0000

    Added dotenv dependancy

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/users');
 var jwt = require('jsonwebtoken');
+require('dotenv').config();
 var validator = require('validator');
 
 /* GET users listing. */
