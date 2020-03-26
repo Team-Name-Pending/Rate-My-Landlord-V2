820afa3d0611d8ffe9c1020b911151864ce62892
MAIRTIN O FLATHARTA(19-09-2019 13-26 <danu7_om1@danu7>
2020-03-26 08:45:33 +0000

    Added basic post retrieval, finding unique houses

--- routes/posts.js
+++ routes/posts.js
@@ -10,20 +10,40 @@ router.post('/addPost', function(req, res, next){
 	var test = req.body.comment.replace(/\s+/g, '');//Remove spaces
 	var test2 = req.body.address.replace(/\s+/g, '');
 	post = new Post(req.body);
-	if(validator.isAlphanumeric(test) && validator.isAlphanumeric(test2)){
-		post.save(function(err, savedPost){
-			if(err)
-				throw err;
-		});
+	try{
+		var jwtString = req.cookies.Authorization.split(" ");
+        var profile = verifyJwt(jwtString[1]);
+		if(profile){
+			if(validator.isAlphanumeric(test) && validator.isAlphanumeric(test2)){
+				post.save(function(err, savedPost){
+					if(err)
+						throw err;
+				});
 		
-		res.json({"response": "Post was saved"});
+				res.json({"response": "Post was saved"});
 			
+			}
+			res.json({"response": "Invalid chars were used"});
+			
+			
+		}
 	}
-	res.json({"response": "Invalid chars were used"});
+	catch(err){
+		res.send(err);
+	}
+	
 });
 
 router.get('/getPosts', function(req, res, next){
-	
+	var mode = req.body.mode;
+	if(mode == "house"){
+		var house = req.body.house;
+		Post.find(house:address, function(err, posts){
+			if(err)
+				res.send(err);
+			res.json(posts);
+		});
+	}
 });
 
 router.delete('/deletePost/:id', function(req, res, next){
@@ -35,11 +55,13 @@ router.delete('/deletePost/:id', function(req, res, next){
 	});	
 });
 
-app.use(multer({ dest: './public/images/',
- rename: function (fieldname, filename) {
-   return filename;
- },
-}));
+router.get('/getHouses/, fuction(req, res, next){
+	Post.find().distinct('address', function(err, ids){
+		if(err)
+			res.send(err);
+		
+	});
+});
 
 app.use(multer({ dest: './public/images/',
  rename: function (fieldname, filename) {
@@ -53,4 +75,10 @@ app.post('/api/photo',function(req,res){
  newItem.img.contentType = 'image/png';
  newItem.save();
 });
+
+function verifyJwt(jwtString) {
+
+    var value = jwt.verify(jwtString, process.env.secret);
+    return value;
+}
 
