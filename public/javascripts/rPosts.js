b3543d4046495ed5f6ed23e86ce3e951e82df3c4
IVANS TISINS(19-09-2019 13-21 <danu7_ti@danu7>
2020-04-08 12:54:32 +0100

    fixed rPosts

--- public/javascripts/rPosts.js
+++ public/javascripts/rPosts.js
@@ -13,7 +13,7 @@ $(document).ready(
 					{
                         posts += "<div class='row justify-content-md-center pt-4'>" +
                             "<div class='card col-md-6'><div class='row'>"
-                            + "<div class='col-md-9'>"+ data[i].user_name+ "<br>"+data[i].comment+"<br> "+data.[i].rating+"</div>" + "<div class='col-md-3'>"+
+                            + "<div class='col-md-9'>"+ data[i].user_name+ "<br>"+data[i].comment+"<br> "+data[i].rating+"</div>" + "<div class='col-md-3'>"+
                             "</div></div></div></div>";
                     }
                     $("#recentPosts").html(posts);
